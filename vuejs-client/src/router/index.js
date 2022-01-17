38b9586514325b0d58bd30b04e0ffece3e1699eb
Shafeeque OM <5572374-shafeequekrews@users.noreply.gitlab.com>
2022-01-17 22:36:22 +0400

    Home page added

--- vuejs-client/src/router/index.js
+++ vuejs-client/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Home from "../views/Entry";
+import Home from "../views/Home";
 import Entry from "../views/Entry";
 import Exit from "../views/Exit";
 import NotFound from "../views/NotFound";
---
+++ vuejs-client/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="text-center museum-text mt-10">MUSEUM OF <br />FUTURE</div>
+    <div class="d-flex justify-center">
+      <v-btn
+        x-large
+        width="150"
+        @click="gotoPage('entry')"
+        height="70"
+        class="ma-2 mr-4"
+        color="#35f3d0"
+      >
+        Entry
+      </v-btn>
+
+      <v-btn
+        x-large
+        width="150"
+        @click="gotoPage('exit')"
+        height="70"
+        color="#ffff95"
+        class="ma-2"
+      >
+        Exit
+      </v-btn>
+
+      <v-btn x-large width="150" height="70" class="ma-2 ml-4" color="#7375ff">
+        List
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    gotoPage(page) {
+      this.$router.push(page);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.museum-text {
+  background: #35f3d0;
+  background: -webkit-linear-gradient(to bottom, #35f3d0 0%, #ffff95 100%);
+  background: -moz-linear-gradient(to bottom, #35f3d0 0%, #ffff95 100%);
+  background: linear-gradient(to bottom, #35f3d0 0%, #ffff95 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 20vh !important;
+  font-weight: 400;
+}
+</style>
